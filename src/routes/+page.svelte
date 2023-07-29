94824180911e63ccae354eb16b3c607cca1f184b
pharmav <yusafme@gmail.com>
2023-07-30 02:25:37 +0800

    ups

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,3 +3,10 @@
 </script>
 
 <HomeWinner />
+<svelte:head>
+	<title>SHTZ docs | SvelteKit + tRPC</title>
+	<meta
+		name="description"
+		content="A library which make the features of SvelteKit and tRPC compliment each other."
+	/>
+</svelte:head>
